7413a1a831ae7e682a4d8c89a62572af53aa67c3
nadbag98 <nadavgat@mail.tau.ac.il>
2022-06-08 11:33:09 +0300

    Added deep linear net model, including experiment and optuna yaml

---
+++ src/models/linear_net_module.py
@@ -0,0 +1,147 @@
+from typing import Any, List
+
+import torch
+from torch.nn import Sequential, Linear, Dropout, MSELoss, ModuleList
+from pytorch_lightning import LightningModule
+from torchmetrics import MinMetric, MeanSquaredError, MeanAbsoluteError
+
+
+class LinearNetModule(LightningModule):
+    def __init__(
+        self,
+        x_size,
+        y_size,
+        hidden_size,
+        num_layers,
+        lr: float = 0.001,
+        weight_decay: float = 0.0005,
+        dropout: float = 0.0,
+    ):
+        super().__init__()
+
+        # this line allows accessing init params with 'self.hparams' attribute
+        # it also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        self.net = ModuleList()
+
+        self.net.append(Linear(x_size, hidden_size))
+        self.net.append(Dropout(p=dropout))
+
+        # we assume num_layers >= 2, if not can use linear regression instead
+        for i in range(num_layers - 2):
+            self.net.append(Linear(hidden_size, hidden_size))
+            self.net.append(Dropout(p=dropout))
+
+        self.net.append(Linear(hidden_size, y_size))
+
+        self.net.double()  # our data is passed in float64 (i.e. double)
+
+        def init_weights(m):
+            if isinstance(m, Linear):
+                torch.nn.init.xavier_uniform_(m.weight)
+
+        # Weight initialization
+        # torch.nn.init.xavier_uniform_(self.net.weight)
+        self.net.apply(init_weights)
+
+        # loss function
+        self.criterion = MSELoss()
+
+        # use separate metric instance for train, val and test step
+        # to ensure a proper reduction over the epoch
+        self.train_mse = MeanSquaredError()
+        self.val_mse = MeanSquaredError()
+        self.test_mse = MeanSquaredError()
+
+        # for logging best so far validation accuracy
+        self.val_mse_best = MinMetric()
+
+    """
+    A LightningModule organizes your PyTorch code into 5 sections:
+        - Computations (init).
+        - Train loop (training_step)
+        - Validation loop (validation_step)
+        - Test loop (test_step)
+        - Optimizers (configure_optimizers)
+
+    Read the docs:
+        https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html
+    """
+
+    def forward(self, x: torch.Tensor):
+        for layer in self.net:
+            x = layer(x)
+        return x
+
+
+    def step(self, batch: Any):
+        x, y = batch
+        logits = self.forward(x)
+        loss = self.criterion(logits, y)
+        preds = logits
+        return loss, preds, y
+
+
+    def training_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.step(batch)
+
+        # log train metrics
+        mse = self.train_mse(preds, targets)
+        self.log("train/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
+        self.log("train/mse", mse, on_step=False, on_epoch=True, prog_bar=True)
+
+        # we can return here dict with any tensors
+        # and then read it in some callback or in `training_epoch_end()` below
+        # remember to always return loss from `training_step()` or else backpropagation will fail!
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def training_epoch_end(self, outputs: List[Any]):
+        # `outputs` is a list of dicts returned from `training_step()`
+        pass
+
+    def validation_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.step(batch)
+
+        # log val metrics
+        mse = self.val_mse(preds, targets)
+        self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
+        self.log("val/mse", mse, on_step=False, on_epoch=True, prog_bar=True)
+
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def validation_epoch_end(self, outputs: List[Any]):
+        mse = self.val_mse.compute()  # get val accuracy from current epoch
+        self.val_mse_best.update(mse)
+        self.log("val/mse_best", self.val_mse_best.compute(), on_epoch=True, prog_bar=True)
+
+    def test_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.step(batch)
+
+        # log test metrics
+        acc = self.test_mse(preds, targets)
+        self.log("test/loss", loss, on_step=False, on_epoch=True)
+        self.log("test/mse", acc, on_step=False, on_epoch=True)
+
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def test_epoch_end(self, outputs: List[Any]):
+        pass
+
+
+    def on_epoch_end(self):
+        # reset metrics at the end of every epoch
+        self.train_mse.reset()
+        self.test_mse.reset()
+        self.val_mse.reset()
+
+    def configure_optimizers(self):
+        """Choose what optimizers and learning-rate schedulers to use in your optimization.
+        Normally you'd need one. But in the case of GANs or similar you might have multiple.
+
+        See examples here:
+            https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html#configure-optimizers
+        """
+        return torch.optim.Adam(
+            params=self.parameters(), lr=self.hparams.lr, weight_decay=self.hparams.weight_decay
+        )

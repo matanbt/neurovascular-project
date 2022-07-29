faf784974a81d12649372697df3588b3be4f2f99
Matan Ben-Tov <63508603+matanbt@users.noreply.github.com>
2022-07-29 16:41:48 +0300

    updated EHRF grid search

--- scripts/grid_search.py
+++ scripts/grid_search.py
@@ -39,26 +39,33 @@ def lin_regr_wind_grid():
 @app.command()
 def ehrf_arch_grid():
     # Grid on possible architectures of ehrf
-    name = 'ehrf_arch_grid'
+    name = 'ehrf_arch_grid_fine'
     datasets = ['2021_02_01_18_45_51_neurovascular_full_dataset', '2021_02_01_19_19_39_neurovascular_full_dataset']
-    with_distances = [True, False]
-    with_vascular_mean = [True, False]
-    with_latent_fcnn = [True, False]
-    all_combinations = itertools.product(datasets, with_distances, with_vascular_mean, with_latent_fcnn)
+    flags_mean = [True, False, False]  # [vascular, distances, fcnn]
+    flags_all = [True, True, True]      # the best model
+    flags_no_dis = [True, False, True]  # (ablates distances)
+    conv_1d_hidden_layers_options = [0, 20, 50]
+    # wind_backs = [1, 2, 5, 7, 10, 13, 15, 20, 30, 50, 70]
+    # wind_forwards = [1, 2, 5, 7, 10, 13, 15, 20, 30, 50, 70]
+    # with_distances = [True, False]
+    # with_vascular_mean = [True, False]
+    # with_latent_fcnn = [True, False]
+    all_combinations = itertools.product(datasets, conv_1d_hidden_layers_options, [flags_all, flags_mean, flags_no_dis])
 
-    for dataset, with_distances, with_vascular_mean, with_latent_fcnn in all_combinations:
+    for dataset, conv_1d_hidden_layers, flags in all_combinations:
+        with_vascular_mean, with_distances, with_latent_fcnn = flags 
         print(f">>> running: dataset={dataset}, with_distances={with_distances}, "
               f"with_vascular_mean={with_vascular_mean}, with_latent_fcnn={with_latent_fcnn}")
         cmd = f"python train.py -m experiment=experiment_ehrf " \
-              f"trainer.gpus=1 logger=wandb trainer.min_epochs=20 trainer.max_epochs=60 " \
+              f"trainer.gpus=1 logger=wandb trainer.min_epochs=40 trainer.max_epochs=100 " \
               f"model.lr=0.000075 " \
               f"datamodule.dataset_object.dataset_name={dataset} " \
               f"datamodule.batch_size=128" \
-              f"datamodule.dataset_object.window_len_neuro_back=15 " \
-              f"datamodule.dataset_object.window_len_neuro_forward=10 " \
+              f"datamodule.dataset_object.window_len_neuro_back=50 " \
+              f"datamodule.dataset_object.window_len_neuro_forward=50 " \
               f"model.with_vascular_mean={with_vascular_mean} " \
               f"model.with_distances={with_distances} " \
-              f"model.conv_1d_hidden_layers=20 " \
+              f"model.conv_1d_hidden_layers={conv_1d_hidden_layers} " \
               f"model.with_latent_fcnn={with_latent_fcnn} " \
               f"model.latent_hidden_layers=2 model.latent_hidden_layer_dim=500 model.latent_hidden_dropout=0.3 " \
               f"name={name}"

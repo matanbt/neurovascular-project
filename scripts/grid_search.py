d73891a53aa64cab3ea642d6795905fa5ffb8ce7
Matan Ben-Tov <63508603+matanbt@users.noreply.github.com>
2022-07-29 17:30:42 +0300

    added rnn grid search

--- scripts/grid_search.py
+++ scripts/grid_search.py
@@ -78,5 +78,42 @@ def ehrf_arch_grid():
     print("DONE")
 
 
+
+
+@app.command()
+def rnn_grid():
+    # Grid on possible architectures of ehrf
+    name = 'rnn_grid'
+    datasets = ['2021_02_01_18_45_51_neurovascular_full_dataset', '2021_02_01_19_19_39_neurovascular_full_dataset']
+    wind_backs = [1, 7, 50]
+    wind_forwards = [1, 7, 50]
+    rnn_model_types = ['LSTM', 'GRU']
+    rnn_bis = [True, False]
+    rnn_layers_counts = [1, 3]
+    all_combinations = itertools.product(datasets, wind_backs, wind_forwards, rnn_model_types, rnn_bis, rnn_layers_counts)
+
+    for dataset, wind_back, wind_forward, rnn_model_type, rnn_bidirectional, rnn_layers_count in all_combinations:
+        print(f">>> running: ", dataset, wind_back, wind_forward, rnn_model_type, rnn_bidirectional, rnn_layers_count)
+        cmd = f"python train.py -m experiment=experiment_rnn " \
+              f"trainer.gpus=1 logger=wandb trainer.min_epochs=40 trainer.max_epochs=200 " \
+              f"datamodule.dataset_object.dataset_name={dataset} " \
+              f"datamodule.batch_size=128 " \
+              f"datamodule.dataset_object.window_len_neuro_back={wind_back} " \
+              f"datamodule.dataset_object.window_len_neuro_forward={wind_forward} " \
+              f"model.rnn_model_type={rnn_model_type} " \
+              f"model.rnn_bidirectional={rnn_bidirectional} " \
+              f"model.rnn_layers_count={rnn_layers_count} " \
+              f"model.rnn_dropout=0.25 model.rnn_hidden_dim=500 " \
+              f"name={name}"
+        print(f"cmd: {cmd}")
+        res = subprocess.run(cmd, shell=True)
+        if res.returncode != 0:
+            print(">>> RUN FAILED!!!")
+            print(res.stderr)
+            break
+
+    print("DONE")
+
+
 if __name__ == '__main__':
     app()

f0541bcdccce2b887614e8f48394736cd16c7313
MatanBT <bentov.matan@gmail.com>
2022-08-07 09:07:37 +0300

    Update grid_search

--- scripts/grid_search.py
+++ scripts/grid_search.py
@@ -50,6 +50,7 @@ def lin_regr_wind_with_vascu_grid():
         print(f">>> running: dataset={dataset}, wind_back={wind_back}, wind_forward={wind_forward}")
         cmd = "python train.py -m experiment=experiment_lin_regr trainer.gpus=1 logger=wandb trainer.min_epochs=50 " \
               "trainer.max_epochs=500 " \
+              "model.lr=0.0005 " \
               f"datamodule.dataset_object.dataset_name={dataset} " \
               "datamodule.batch_size=128 " \
               f"datamodule.dataset_object.window_len_neuro_back={wind_back}" \
